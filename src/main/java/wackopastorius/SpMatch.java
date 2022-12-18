0bd5e54e06187e82ff223609803aae2aad3e526c
RobAnd90 <robin.andersson@edu.edugrade.se>
2022-12-18 18:04:36 +0100

    Added methods

--- src/main/java/wackopastorius/SpMatch.java
+++ src/main/java/wackopastorius/SpMatch.java
@@ -2,7 +2,7 @@ package wackopastorius;
 
 /*
 *
-* Author so far: Mikael Petersson
+* Author so far: Robin Andersson
 *
 * */
 
@@ -10,22 +10,22 @@ public class SpMatch {
 
     private Player[] players;
 
-    public SpMatch() {
-    }
+    public SpMatch() {}
 
     public SpMatch(Player[] players) {
-        this.players = players;
-    }
+        this.players = players;}
 
-    public Player[] getPlayers() {
-        return players;
-    }
+    public Player[] getPlayers() {return players;}
 
-    public void setPlayers(Player[] players) {
-        this.players = players;
-    }
+    public void setPlayers(Player[] players) {this.players = players;}
 
     //Methods
 
+    public void choosePlayer1(Player player1) {
+        player1.getNickname();
+    }
 
+    public void choosePlayer2(Player player2) {
+        player2.getNickname();
+    }
 }
